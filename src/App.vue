a1a71f3572aeaefdb0ff70104ab6a988286cc0de
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-20 12:56:25 +0200

    Add item list example

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 
+
+let message = ref('Hello Vue');
+let items = ref(['Sai', 'Munad', 'Piim', 'Viin', 'saa']);
+function add(){
+  if(message.value.trim() !== ''){
+    items.value.push(message.value.trim());
+  }
+  message.value = '';
+}
 </script>
 
 <template>
-
+<button @click="add">Click me</button>
+<input type="text" v-model="message" @keydown.enter="add"> 
+<ul>
+  <li v-for="item in items">{{ item }}</li>
+</ul>
 </template>
 
